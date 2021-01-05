4c566c2e00783ee3dfa96c67ffbcc8afbeedff97
ValdirCezar <valdircezar312@gmail.com>
2021-01-05 11:06:54 -0300

    ClienteDTO implementado e ClienteResource iniciado

---
+++ src/main/java/com/valdir/jwt/dtos/ClienteDTO.java
@@ -0,0 +1,70 @@
+package com.valdir.jwt.dtos;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.valdir.jwt.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	@NotEmpty(message = "Campo NOME é mandatório")
+	private String nome;
+
+	@NotEmpty(message = "Campo EMAIL é mandatório")
+	@Email
+	private String email;
+
+	@NotEmpty(message = "Campo SENHA é mandatório")
+	private String senha;
+
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
---
+++ src/main/java/com/valdir/jwt/resources/ClienteResource.java
@@ -0,0 +1,12 @@
+package com.valdir.jwt.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class ClienteResource {
+	
+	
+
+}
