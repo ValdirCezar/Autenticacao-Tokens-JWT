e0bb789c0270598788c66368cd95a9ae1d1c01dc
ValdirCezar <valdircezar312@gmail.com>
2021-01-07 14:57:44 -0300

    Classe CredenciaisDTO criada

---
+++ src/main/java/com/valdir/jwt/dtos/CredenciaisDTO.java
@@ -0,0 +1,38 @@
+package com.valdir.jwt.dtos;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String email;
+	private String senha;
+
+	public CredenciaisDTO(String email, String senha) {
+		super();
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public CredenciaisDTO() {
+		super();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
