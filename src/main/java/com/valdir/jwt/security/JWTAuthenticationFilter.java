b998d78b7727da24df7632ebf6038713f70e1e73
ValdirCezar <valdircezar312@gmail.com>
2021-01-08 08:30:05 -0300

    ajustes na responsa em JWTAuthenticationFilter

--- src/main/java/com/valdir/jwt/security/JWTAuthenticationFilter.java
+++ src/main/java/com/valdir/jwt/security/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.valdir.jwt.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -96,6 +98,27 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) authResult.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		response.addHeader("Authorization", "Bearer " + token);
+	}
+	
+	private class JwtAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+		@Override
+		public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+				AuthenticationException exception) throws IOException, ServletException {
+			response.setStatus(401);
+			response.setContentType("application/json");
+			response.getWriter().append(json());
+			
+		}
+
+		private String json() {
+			long date = new Date().getTime();
+			return "{\"timestamp\": " + date + ", " + "\"status\": 401, " + "\"error\": \"Não autorizado\", "
+					+ "\"message\": \"Email ou senha inválidos\", " + "\"path\": \"/login\"}";
+		}
+		
+		
+		
 	}
 
 }
